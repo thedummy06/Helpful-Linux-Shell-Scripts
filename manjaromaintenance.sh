03440307203ee7ae6b440194063bd6053487b45a
thedummy06 <js185r@gmail.com>
2017-01-17 01:14:03 -0600

    Add files via upload

---
+++ manjaromaintenance.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+#This will try to ensure you have a strong network connection
+for c in computer;
+do 
+sudo ping -c4 google.com 
+if [ $? -eq 0 ]
+then 
+	echo "Connection successful"
+else
+sudo systemctl stop NetworkManager.service
+sudo systemctl disable NetworkManager.service
+sudo systemctl enable NetworkManager.service
+sudo systemctl start NetworkManager.service
+sudo ifconfig up eth0
+sudo dhclient eth0
+fi
+done
+
+#This will give useful system information for troubleshooting certain issues
+mkdir troubleshooting
+cd troubleshooting
+df -h >> analysis.txt
+free -h >> analysis.txt
+uname -a >> analysis.txt
+sudo blkid >> analysis.txt
+lsblk >> analysis.txt
+lsb_release -a >> analysis.txt
+sudo ps aux >> analysis.txt
+ifconfig >> analysis.txt
+Xorg -version >> analysis.txt
+ldd --version >> analysis.txt
+tail /var/log/dmesg >> dmesg.txt
+tail /var/log/syslog >> syslog.txt
+sudo lshw >> hardware.txt
+sudo lspci >> lspci.txt
+sudo lsusb >> lsusb.txt
+cd
+
+#This will reload the firewall to ensure it's enabled
+sudo systemctl disable ufw
+sudo systemctl enable ufw 
+sudo ufw enable
+
+#This will clean the cache
+sudo rm -r .cache/*
+sudo rm -r .thumbnails/*
+sudo rm -r ~/.local/share/Trash
+
+#This will remove orphan packages from pacman 
+#sudo pacman -Rs $(pacman -Qqdt)
+
+#Optional This will remove the pamac cached applications and older versions
+echo "You rarely should ever do this"
+echo "Do you want to remove the package cache from pamac? (Y/n)"
+read answer 
+if [[ $answer == Y ]];
+then 
+sudo pacman -Sc
+fi
+
+#This will ensure you are up to date and running fastest mirrors 
+sudo pacman-mirrors -g
+sudo pacman-optimize && sync
+sudo pacman -Syyu
+
+#This will set up screenfetch
+sudo pacman -S screenfetch
+sudo cp /etc/bash.bashrc /etc/bash.bashrc.bak
+echo "screenfetch" | sudo tee -a /etc/bash.bashrc
+
+#This updates the hosts file:Optional
+#sudo ./hostsman4linux.sh
+
+#This will help with configuration file mismatches
+sudo etc-update
+
+#This adds screen fetch to the new bash.bashrc file
+echo "screenfetch" | sudo tee -a /etc/bash.bashrc
+
+#This runs a disk checkup and attempts to fix filesystem
+sudo touch /forcefsck 
+
+#You can run bleachbit
+echo "You may wish to run bleachbit for other trash and unneeded locales"
+echo "Run bleachbit? (Y/n)"
+read answer 
+if [[ $answer == Y ]]; 
+then bleachbit
+fi
+
+#This refreshes systemd in case of new or failed units
+sudo systemctl daemon-reload
+
+#This aids in diagnosing systemd specific issues
+systemctl status >> systemddiagnostics.txt
+systemctl >> systemddiagnostics.txt
+systemctl --failed >> systemddiagnostics.txt
+
+#Sometimes it's good to check for broken symlinks
+echo "Checking for broken symlinks."
+echo "When convenient, refer back to sym.txt for the links."
+find -xtype l -print >> sym.txt
+
+#This will reboot the system
+sudo systemctl reboot
---
+++ manjaronvidia-installer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#This checks your device
+sudo mhwd -li
+sleep 1
+
+echo "What would you like to do?"
+
+echo "1-Proprietary driver"
+echo "2-default driver"
+echo "3-exit"
+
+read operation;
+
+case $operation in
+	1) sudo mhwd -a pci nonfree 0300 && sudo nvidia-xconfig ;;
+	2) sudo mhwd -r pci video-nvidia ;;
+	3) echo "Goodbye!" ;;
+esac
+
+echo "Would you like to reboot now? (y/n)"
+read answer 
+if [[ $answer == y ]];
+then 
+sudo systemctl reboot
+else 
+echo "Please reboot if you installed a new driver."
+fi
