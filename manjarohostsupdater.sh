5053e0d9e575a77f6e5ad3580a1c611e27d25921
James <js185r@gmail.com>
2017-01-17 22:34:41 -0600

    Update manjarohostsupdater.sh

--- manjarohostsupdater.sh
+++ manjarohostsupdater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #This updates the hosts file
-mv /etc/hosts.bak /etc/hosts
+mv /etc/hosts.bak /etc/hosts #Really only matters the second time and thereafter.
 cp /etc/hosts /etc/hosts.bak
-#cd /home/$USER #If you run as cron job.
+#cd /home/$USER #If you run as cron job, uncomment this line.
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
 cat list >> hosts
 cat hosts >> /etc/hosts 
