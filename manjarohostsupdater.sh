4503422833dbad11d78f4f7a359259dbc63efb65
James <js185r@gmail.com>
2017-01-18 19:49:49 -0600

    Update manjarohostsupdater.sh
    removed the list file for now, will probably add later my own small hosts list to add to the hosts, in which case, you will need to cd into home to build it.

--- manjarohostsupdater.sh
+++ manjarohostsupdater.sh
@@ -4,7 +4,6 @@ mv /etc/hosts.bak /etc/hosts #Really only matters the second time and thereafter
 cp /etc/hosts /etc/hosts.bak
 #cd /home/$USER #If you run as cron job, uncomment this line.
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
-cat list >> hosts
 cat hosts >> /etc/hosts 
 systemctl restart NetworkManager.service
 rm hosts 
