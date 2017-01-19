57cc41c94320ef3e11c6ba8dd325870572a743cf
James <js185r@gmail.com>
2017-01-19 03:41:34 -0600

    Update ubuntusetup.sh

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -137,9 +137,9 @@ echo "archey" | sudo tee -a /etc/bash.bashrc
 #Optional, but it is highly recommended that you make a quick backup
 #The backup directory can be found in your root folder, unless you specify #otherwise 
 #cd /
-#sudo tar -cvpzf backup.tgz --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/tmp --exclude=/run --exclude=/mnt --exclude=/media --exclude=/lost+found --exclude=/backup.tgz / 
+#sudo tar -cvpzf backup.tgz --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/tmp --exclude=/run --exclude=/mnt --exclude=/media --exclude=/lost+found --exclude=/backup.tgz
 #Or for home folder only
-#sudo tar -cvpzf /home/USER/ /
+#sudo tar -cvpzf backuphome.tgz /home/USER/ 
 
 #Let's reboot
 sudo shutdown -r now
