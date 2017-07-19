9bf02bfac10be56f60e3edb7c2381fa88eb9182d
James <js185r@gmail.com>
2017-07-19 11:46:39 -0500

    Add files via upload
    Renamed some files to prevent confusion

---
+++ hostsman4ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This updates the hosts file
+mv /etc/hosts.bak /etc/hosts
+cp /etc/hosts /etc/hosts.bak
+#cd /home/$USER #If you run as cron job.
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
+cat list >> hosts
+cat hosts >> /etc/hosts 
+/etc/init.d/network-manager restart
+rm hosts 
+cat /etc/hosts >> hosts.log
+
+
