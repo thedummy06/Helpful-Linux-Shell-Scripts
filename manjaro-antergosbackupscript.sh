db6fb9bf08e11796474e8b3a4dfd27a162ab0bda
James <js185r@gmail.com>
2017-08-03 02:00:53 -0500

    Update manjaro-antergosbackupscript.sh
    cleaned up and fixed mistakes

--- manjaro-antergosbackupscript.sh
+++ manjaro-antergosbackupscript.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-sudo mv Wallpapers /tmp 
 sudo rm -r .cache/*
 sudo rm  -r .thumbnails/*
 sudo rm -r ~/.local/share/Trash
 #sudo pacman -Sc
 history -c
 cd / 
-sudo rsync -aAXv --exclude=dev --exclude=proc --exclude=Backup --exclude=sys --exclude=tmp --exclude=run --exclude=mnt --exclude=media --exclude=lost+found / /Backup
-cd tmp 
-sudo mv Wallpapers /home/$USER/
+sudo rsync -aAXv --exclude=dev --exclude=proc --exclude=Backup --exclude=Music --exclude=sys --exclude=tmp --exclude=run --exclude=mnt --exclude=media --exclude=lost+found / /Backup
