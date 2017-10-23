547d3fa55eb88679e4b91ef5520ae98bdae1e54d
James <js185r@gmail.com>
2017-10-23 01:34:12 -0500

    Update manjarosetup.sh
    Now installs Chromium automatically

--- manjarosetup.sh
+++ manjarosetup.sh
@@ -88,7 +88,7 @@ sudo pacman -S --noconfirm hardinfo lshw hdparm hddtemp xsensors wget geany
 #sudo pacman -S --noconfirm kdenlive 
 #sudo pacman -S --noconfirm palemoon-bin 
 #sudo pacman -S --noconfirm epiphany 
-#sudo pacman -S --noconfirm chromium 
+sudo pacman -S --noconfirm chromium 
 #sudo pacman -S --noconfirm opera
 #sudo pacman -S --noconfirm transmission-gtk 
 #sudo pacman -S --noconfirm seamonkey 
