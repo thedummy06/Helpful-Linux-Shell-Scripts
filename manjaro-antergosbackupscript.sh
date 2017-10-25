1870fd6f1cec77ce049f0c310904f84c0d88b31e
James <js185r@gmail.com>
2017-10-25 07:04:43 -0500

    Delete manjaro-antergosbackupscript.sh
    Not really needed as is in setup and maintenance scripts.

--- manjaro-antergosbackupscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo rm -r .cache/*
-sudo rm  -r .thumbnails/*
-sudo rm -r ~/.local/share/Trash
-#sudo pacman -Sc
-history -c
-cd / 
-sudo rsync -aAXv --exclude=dev --exclude=proc --exclude=Backup --exclude=Music --exclude=sys --exclude=tmp --exclude=run --exclude=mnt --exclude=media --exclude=lost+found / /Backup
