3aae2932878daf4dddc505210036e90edcb612d5
James <js185r@gmail.com>
2017-10-29 10:40:00 -0500

    Add files via upload
    Added HPGURU'S Hosts file from hosts-file.net(Malwarebytes affiliated). This is a big file, so may cause slow downs on some systems with a dns caching service running.

---
+++ hostsman4manjaro.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This updates the hosts file
+mv /etc/hosts.bak /etc/hosts
+cp /etc/hosts /etc/hosts.bak
+cd /home/james #If you run as cron job.
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts
+wget http://www.montanamenagerie.org/hostsfile/hosts.txt
+wget https://hosts-file.net/hphosts-partial.txt
+wget http://sysctl.org/cameleon/hosts -O cameleonhosts
+sed -i 's/127.0.0.1/0.0.0.0/g' hosts.txt
+sed -i 's/127.0.0.1/0.0.0.0/g' hphosts-partial.txt
+sed -i 's/127.0.0.1/0.0.0.0/g' cameleonhosts
+cat hosts.txt >> hosts
+cat hphosts-partial.txt >> hosts
+cat cameleonhosts >> hosts
+cat Jameshostslist >> hosts
+sort -u hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+uniq -u hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts 
+sed -i '1,19d' hosts
+grep -v "1337x.to" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+cat hosts >> /etc/hosts 
+systemctl restart NetworkManager.service
+rm hosts hphosts-partial.txt hosts.txt cameleonhosts
+cat /etc/hosts >> hosts.log
