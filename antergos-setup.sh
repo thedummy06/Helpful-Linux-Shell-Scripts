e8823aa297ed1c8a2574cc3d1cfdbba8146afd59
James <js185r@gmail.com>
2017-11-21 02:53:54 -0600

    Update antergos-setup.sh
    Added pacaur helper to optional programs.

--- antergos-setup.sh
+++ antergos-setup.sh
@@ -95,6 +95,7 @@ read -p "Press Enter to continue."
 #This will install a few useful apps
 sudo pacman -S --noconfirm bleachbit reflector gnome-disk-utility ncdu nmap hardinfo lshw iotop htop inxi rkhunter transmission-gtk hdparm hddtemp xsensors geany dhclient
 #Optional 
+#sudo pacman -S --noconfirm pacaur
 #sudo pacman -S --noconfirm redshift
 #sudo pacman -S --noconfirm blender
 #sudo pacman -S --noconfirm qbittorrent
