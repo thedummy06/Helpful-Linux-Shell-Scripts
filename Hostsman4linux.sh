3df3f3bba58c3a8858db5f5cb905003f177826c5
James <js185r@gmail.com>
2018-03-11 10:22:30 -0500

    Add files via upload
    Changed nocoin to coinblocker from zerodot aka the same list that is used in Steven Black's hosts

--- Hostsman4linux.sh
+++ Hostsman4linux.sh
@@ -37,13 +37,13 @@ then
 elif [[ $package -eq 3 ]];
 then
 	wget http://hosts-file.malwareteks.com/hosts.txt -O hosts
-	wget https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt -O nocoin
+	wget raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblocker
 	wget https://hosts-file.net/hphosts-partial.txt
 	wget http://sysctl.org/cameleon/hosts -O cameleonhosts
 	cat hphosts-partial.txt >> hosts
 	cat cameleonhosts >> hosts
-	cat nocoin >> hosts
-	rm hphosts-partial.txt cameleonhost nocoin
+	cat coinblocker >> hosts
+	rm hphosts-partial.txt cameleonhost coinblocker
 elif [[ $package -eq 4 ]];
 then 
 	wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts
@@ -63,7 +63,7 @@ then
 	wget https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts -O adservers.txt
 	wget http://winhelp2002.mvps.org/hosts.txt -O MVPShosts
 	wget someonewhocares.org/hosts/hosts
-	wget https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt -O nocoin
+	wget raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblocker
 	wget https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/snuff-hosts -O Pron2
 	wget https://raw.githubusercontent.com/marktron/fakenews/master/fakenews
 	wget https://raw.githubusercontent.com/Sinfonietta/hostfiles/master/gambling-hosts -O Gamblinglist
@@ -84,7 +84,7 @@ then
 	wget http://sysctl.org/cameleon/hosts -O cameleonhosts
 	wget https://raw.githubusercontent.com/zant95/hmirror/master/data/malwaredomains.com-justdomains/list.txt -O Malwarehosts2 && sed -i 's/^/0.0.0.0  /' Malwarehosts2
 	cat MVPShosts >> hosts
-	cat nocoin >> hosts
+	cat coinblocker >> hosts
 	cat Malwarehosts >> hosts
 	cat Pron >> hosts
 	cat Pron2 >> hosts
@@ -105,17 +105,17 @@ then
 	cat hphosts-partial.txt >> hosts
 	cat cameleonhosts >> hosts
 	cat Malwarehosts2 >> hosts
-	rm KADhosts.txt MVPShosts nocoin Malwarehosts Malwarehosts2 add.Spam add.Dead add.Risk add.2o7Net Badd-Boyz tyzbit adservers.txt hphosts-partial.txt hphosts cameleonhosts unchecky spamhosts Stevenhosts Pron Pron2 Gamblinglist fakenews
+	rm KADhosts.txt MVPShosts coinblocker Malwarehosts Malwarehosts2 add.Spam add.Dead add.Risk add.2o7Net Badd-Boyz tyzbit adservers.txt hphosts-partial.txt hphosts cameleonhosts unchecky spamhosts Stevenhosts Pron Pron2 Gamblinglist fakenews
 elif [[ $package -eq 6 ]];
 then
 	echo "This could block sites that you need, you've been warned."
 	sleep 1
 	wget hosts-file.net/ad_servers.txt
-	wget https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt -O nocoin
+	wget raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblocker
 	wget https://raw.githubusercontent.com/joeylane/hosts/master/hosts # Does block google
 	cat ad_servers.txt >> hosts
-	cat nocoin >> hosts
-	rm ad_servers.txt nocoin
+	cat coinblocker >> hosts
+	rm ad_servers.txt coinblocker
 	#grep -v "Google.com" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts #This unblocks google.com outright
 elif [[ $package -eq 7 ]];
 then
@@ -123,15 +123,15 @@ then
 	wget https://github.com/mitchellkrogza/Ultimate.Hosts.Blacklist/blob/master/hosts.zip?raw=true
 	unzip 'hosts.zip?raw=true'
 	mv hosts.txt hosts
-	wget https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt -O nocoin
-	cat nocoin >> hosts
-	rm'hosts.zip?raw=true' nocoin
+	wget raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblocker
+	cat coinblocker >> hosts
+	rm'hosts.zip?raw=true' coinblocker
 elif [[ $package -eq 8 ]];
 then
 #Umatrix style formula with some extra
 	wget hosts-file.net/ad_servers.txt
-	wget https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt -O nocoin
 	wget someonewhocares.org/hosts/hosts
+	wget raw.githubusercontent.com/ZeroDot1/CoinBlockerLists/master/hosts -O coinblocker
 	wget http://sysctl.org/cameleon/hosts -O cameleonhosts
 	wget http://winhelp2002.mvps.org/hosts.txt -O MVPShosts
 	wget http://www.malwaredomainlist.com/hostslist/hosts.txt -O Malwarehosts
@@ -145,8 +145,8 @@ then
 	cat cameleonhosts >> hosts
 	cat ad_servers.txt >> hosts
 	cat Spamhosts >> hosts
-	cat nocoin >> hosts
-	rm ad_servers.txt Petersadslist nocoin Malwarehosts Malware2 Spamhosts MVPShosts cameleonhosts
+	cat coinblocker >> hosts
+	rm ad_servers.txt Petersadslist coinblocker Malwarehosts Malware2 Spamhosts MVPShosts cameleonhosts
 else 
 	echo "Run again and pick a valid number."
 	exit
@@ -176,12 +176,17 @@ echo "Are there any other domains that you wish to exclude?(Y/n)"
 read answer
 while [ $answer == Y ]
 do
-	read -p "Enter any other domains you wish to exclude up to 5:" Domain1 Domain2 Domain3 Domain4 Domain5
+	read -p "Enter any other domains you wish to exclude up to 10:" Domain1 Domain2 Domain3 Domain4 Domain5 Domain6 Domain7 Domain8 Domain9 Domain10
 	grep -v "$Domain1" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
 	grep -v "$Domain2" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
 	grep -v "$Domain3" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
 	grep -v "$Domain4" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
 	grep -v "$Domain5" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+	grep -v "$Domain6" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+	grep -v "$Domain7" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+	grep -v "$Domain8" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+	grep -v "$Domain9" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
+	grep -v "$Domain10" hosts > /tmp/hosts.new && mv /tmp/hosts.new hosts
 break
 done
 
